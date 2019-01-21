429f467f4bd1bb6e61b45770628c36f6e7edbe3d
Albert Martinez <albertstartup@gmail.com>
2019-01-21 06:45:16 -0800

    Add build folder.

---
+++ build/precache-manifest.b96b44ed48da58195ee44faa99272bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53a6e11a4164d31cfc19",
+    "url": "/static/css/main.e2744b64.chunk.css"
+  },
+  {
+    "revision": "53a6e11a4164d31cfc19",
+    "url": "/static/js/main.53a6e11a.chunk.js"
+  },
+  {
+    "revision": "167b6ab434d60af09543",
+    "url": "/static/js/1.167b6ab4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0d6e1b8c32a312bc7b199865bc07ac82",
+    "url": "/index.html"
+  }
+];
