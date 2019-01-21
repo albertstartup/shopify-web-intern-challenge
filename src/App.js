83cc02b79ee411ef8631a9036b68524f1e65b3f5
Albert Martinez <albertstartup@gmail.com>
2019-01-21 08:50:57 -0800

    Get fav working.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import request from 'request-promise-native';
+import 'semantic-ui-css/semantic.min.css'
 import { Input } from 'semantic-ui-react';
 import _ from 'underscore';
+import {Icon} from 'semantic-ui-react';
+
 window._ = _;
 
 class App extends Component {
@@ -14,13 +17,17 @@ class App extends Component {
       items: [],
       searchInput: '',
       favItems: [],
-      itemsToDisplay: []
+      itemsToDisplay: [],
+      faves: []
     };
 
     window.findme = this;
   }
 
   onChangeSearch(e) {
+    if (e.target.value === '') {
+      this.setState({itemsToDisplay: []});
+    }
     this.setState({searchInput: e.target.value});
     console.log()
   }
@@ -30,7 +37,7 @@ class App extends Component {
   }
 
   async componentDidMount() {
-
+    this.setState({faves: JSON.parse(localStorage.getItem("faves")) || []});
   }
 
   async onSubmitSearch() {
@@ -40,7 +47,8 @@ class App extends Component {
     console.log(items);
     const itemsToDisplay = _.filter(items, (item) => {
       const keywords = item.keywords;
-      if (keywords.indexOf(this.state.searchInput) !== -1) {
+      const title = item.title;
+      if (keywords.indexOf(this.state.searchInput) !== -1 || title.indexOf(this.state.searchInput) !== -1) {
         return true;
       }
     });
@@ -51,6 +59,22 @@ class App extends Component {
     const textarea  = document.createElement("textarea");
     textarea.innerHTML = encodedString;
     return textarea.value;
+  }
+
+  onClickStar(item) {
+    const newFaves = this.state.faves.concat(item);
+    this.setState({faves: newFaves})
+    localStorage.setItem("faves", JSON.stringify(newFaves))
+  }
+
+  onClickFavStar(item) {
+    const newFaves = _.without(this.state.faves, item);
+    this.setState({faves: newFaves});
+    localStorage.setItem("faves", JSON.stringify(newFaves))
+  }
+
+  getStarColor(title) {
+    return _.includes(this.state.faves, title) ? 'green' : 'grey';
   }
 
   render() {
@@ -66,14 +90,33 @@ class App extends Component {
           this.onSubmitSearch();
         }} type='submit'/>
       </form>
-
-        {_.map(this.state.itemsToDisplay, (item) => {
-          return <div key={item.title} style={{display: 'flex'}}>
-            <div style={{flex: 1, padding: '1rem'}}>{item.title}</div>
-            <div style={{flex: 1, padding: '1rem'}} dangerouslySetInnerHTML={{__html: this.getDecodedString(item.body)}}>
+      {_.map(this.state.itemsToDisplay, (item) => {
+        return <div key={item.id} style={{display: 'flex'}}>
+          <div style={{flex: 1, padding: '1rem'}}>
+            <div style={{display: 'flex', alignItems: 'baseline'}}>
+              <Icon onClick={() => {
+                this.onClickStar(item);
+              }} color={this.getStarColor(item)} size='small' name='star'/>{item.title}
+              </div>
             </div>
+          <div style={{flex: 1, padding: '1rem'}} dangerouslySetInnerHTML={{__html: this.getDecodedString(item.body)}}>
+          </div>
         </div>
-        })}
+      })}
+
+      {_.map(this.state.faves, (item) => {
+        return <div key={item.id} style={{display: 'flex'}}>
+          <div style={{flex: 1, padding: '1rem'}}>
+            <div style={{display: 'flex', alignItems: 'baseline'}}>
+              <Icon onClick={() => {
+                this.onClickFavStar(item);
+              }} color='green' size='small' name='star'/>{item.title}
+            </div>
+          </div>
+          <div style={{flex: 1, padding: '1rem'}} dangerouslySetInnerHTML={{__html: this.getDecodedString(item.body)}}>
+          </div>
+        </div>
+      })}
 
     </div>
   }
