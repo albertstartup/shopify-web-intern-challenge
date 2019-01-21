7e95e5acce411e367ae1ef5346b9bfaba6b53e48
Albert Martinez <albertstartup@gmail.com>
2019-01-21 05:40:01 -0800

    Add all files.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import request from 'request-promise-native';
+import { Input } from 'semantic-ui-react';
+import _ from 'underscore';
+window._ = _;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      searchInput: '',
+      favItems: [],
+      itemsToDisplay: []
+    };
+
+    window.findme = this;
+  }
+
+  onChangeSearch(e) {
+    this.setState({searchInput: e.target.value});
+    console.log()
+  }
+
+  async getItems() {
+    return await request.get('https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000', {json: true});
+  }
+
+  async componentDidMount() {
+
+  }
+
+  async onSubmitSearch() {
+    console.log('seach submited');
+    const items = await this.getItems();
+    this.setState({items: items});
+    console.log(items);
+    const itemsToDisplay = _.filter(items, (item) => {
+      const keywords = item.keywords;
+      if (keywords.indexOf(this.state.searchInput) !== -1) {
+        return true;
+      }
+    });
+    this.setState({itemsToDisplay: itemsToDisplay});
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return <div>
+      <form
+          onChange={(e) => {
+            this.onChangeSearch(e);
+          }}
+          style={{display: 'flex'}}>
+        <input style={{flex: '2'}}type='text'/>
+        <input onClick={(e) => {
+          e.preventDefault();
+          this.onSubmitSearch();
+        }} type='submit'/>
+      </form>
+      <div style={{display: 'flex'}}>
+        <div>1</div>
+        <div>2</div>
       </div>
-    );
+    </div>
   }
 }
 
